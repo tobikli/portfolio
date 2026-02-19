dc857706af6a7c24dacdd0048a303592426600e3
Tobias Klingenberg <tobikli@pm.me>
2026-02-19 20:43:50 +0100

    Fix blend mode cursor

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-import { useDark } from '@vueuse/core'
 import CustomCursor from './ui-components/CustomCursor.vue'
 import { RouterView } from 'vue-router'
-const isDark = useDark()
 
 </script>
 
 <template>
   <custom-cursor
     :targets="['img', 'a', 'button', 'cursor-hover']"
-    :circleColor="isDark ? '#fff' : '#000'"
-    :circleColorHover="isDark ? '#fff' : '#000'"
-    :dotColor="isDark ? '#fff' : '#000'"
-    :dotColorHover="isDark ? '#fff' : '#000'"
+    :circleColor="'#fff'"
+    :circleColorHover="'#fff'"
+    :dotColor="'#fff'"
+    :dotColorHover="'#fff'"
     :hoverSize="1"
   />
   <RouterView />
